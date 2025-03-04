5bfddcfc947b174f37f6b206665f0a02dbd1415c
007kumar <shiv.rv007@gmail.com>
2025-03-05 00:33:55 +0530

    Update index.js

--- backend/helloService/index.js
+++ backend/helloService/index.js
@@ -1,20 +1,21 @@
-const express = require('express');
-require('dotenv').config()
-var cors = require('cors')
-
+  GNU nano 7.2                                                           index.js                                                                    const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
-app.use(cors())
+const PORT = process.env.PORT || 3000;
 
-
+app.use(cors());
 app.use(express.json());
-app.get('/', (req,res)=>{
-    res.send({msg: 'Hello World'})
-})
-app.get('/health', (req,res)=>{
-    res.send({status: 'OK'})
-})
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+app.get('/', (req, res) => {
+    res.send({ msg: 'Hello World' });
+});
+
+app.get('/health', (req, res) => {
+    res.send({ status: 'OK' });
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
 });
